c57cc83dbce96f93e5f2dd15ffb0e7ef1b2486d2
TheZeorymer <alzaru4@gmail.com>
2025-05-23 12:53:06 +0530

    new2

--- moonbot_engine.pyx
+++ moonbot_engine.pyx
@@ -4,10 +4,6 @@ cimport numpy as np
 from libc.stdint cimport uint64_t
 
 cdef class BitboardMoonBot:
-    cdef np.ndarray[np.uint64_t, ndim=1] white_pieces
-    cdef np.ndarray[np.uint64_t, ndim=1] black_pieces
-    cdef int turn  # 0=white, 1=black
-
     def __init__(self):
         self.white_pieces = np.zeros(6, dtype=np.uint64)
         self.black_pieces = np.zeros(6, dtype=np.uint64)
@@ -15,11 +11,15 @@ cdef class BitboardMoonBot:
         # TODO: initialize bitboards for starting position
 
     cpdef int evaluate(self):
+        cdef np.ndarray[np.uint64_t, ndim=1] white = self.white_pieces
+        cdef np.ndarray[np.uint64_t, ndim=1] black = self.black_pieces
         cdef int score = 0
         # TODO: implement fast bitwise evaluation
         return score
 
     cpdef tuple negamax(self, int depth, int alpha, int beta):
+        cdef np.ndarray[np.uint64_t, ndim=1] white = self.white_pieces
+        cdef np.ndarray[np.uint64_t, ndim=1] black = self.black_pieces
         # TODO: implement fast bitboard negamax search
         return (0, None)
 
